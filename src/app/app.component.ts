5efc01c2b980e3b726737bbcab66c26c2c17b325
RolandOroz <rolandoroz@gmail.com>
2022-04-21 00:25:19 +0200

    making @Input and @Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'User List';
+
+  users = [
+    {
+      id: '1',
+      name: 'Jack',
+      age: 21
+    },
+    {
+      id: '2',
+      name: 'John',
+      age: 25
+    },
+    {
+      id: '3',
+      name: 'Sam',
+      age: 29
+    }
+  ];
+
+  removeUser(id: string): void {
+    // deleting user by id
+    this.users = this.users.filter(user => user.id !== id);
+  }
+
 }
--- src/app/usersList/usersList.component.ts
+++ src/app/usersList/usersList.component.ts
@@ -1,39 +1,48 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './usersList.component.html',
+  styleUrls: ['usersList.component.scss']
 })
 
 export class UsersListComponent {
+//passing data from parent to child
+  @Input() users;
+//passing data from child to parent
+  @Output() removeUser = new EventEmitter();
+
   newUserName = '';
-  users = [
-    {
-      id: '1',
-      name: 'Jack',
-      age: 21
-    },
-    {
-      id: '2',
-      name: 'John',
-      age: 25
-    },
-    {
-      id: '3',
-      name: 'Sam',
-      age: 29
-    }
-  ];
+  // moving to app.component.ts and using @Input instead
+  // users = [
+  //   {
+  //     id: '1',
+  //     name: 'Jack',
+  //     age: 21
+  //   },
+  //   {
+  //     id: '2',
+  //     name: 'John',
+  //     age: 25
+  //   },
+  //   {
+  //     id: '3',
+  //     name: 'Sam',
+  //     age: 29
+  //   }
+  // ];
 
-  removeUser(id: string): void {
- // deleting user by id
-    this.users = this.users.filter(user => user.id !== id);
-  }
-setNewUserName(userName: string): void {
+// moving to app.component.ts and using @Output instead
+//   removeUser(id: string): void {
+//     // deleting user by id
+//     this.users = this.users.filter(user => user.id !== id);
+//   }
+
+  setNewUserName(userName: string): void {
     this.newUserName = userName;
-}
+  }
 
-addUser(): void {
+  addUser(): void {
     const uniqueId = Math.random().toString(16);
     const newUser = {
       id: uniqueId,
@@ -43,6 +52,6 @@ addUser(): void {
     this.users.push(newUser);
 // sets value to empty string
     this.newUserName = '';
-}
+  }
 }
 
